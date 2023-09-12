838d2d4a78011ab6fb212b291fa9cac549484ec8
Paul OHL <paul.lv.ohl@gmail.com>
2023-09-12 12:28:19 +0200

    Added an index page with qr-code generation

---
+++ sign.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Names Selector</title>
+</head>
+<body>
+    <label for="nameSelector">Select a name:</label>
+    <select id="nameSelector">
+        <option value="John">John</option>
+        <option value="Jane">Jane</option>
+        <option value="Bob">Bob</option>
+        <option value="Alice">Alice</option>
+    </select>
+</body>
+</html>
+
