178dbc2a7b9eae84532b313130124d462d1a0426
Paul OHL <paul.lv.ohl@gmail.com>
2023-09-14 16:40:58 +0200

    cleanup

--- resources/views/session.blade.php
+++ resources/views/session.blade.php
@@ -15,46 +15,38 @@
 </div>
 
 <script type="text/javascript">
-    let text = "http://{{ env('APP_URL') }}:8000/sign/{{ $session->id }}"
-    function generateQRCode() {
-        let qrcodeContainer = document.getElementById("qr-code");
-        qrcodeContainer.innerHTML = "";
-        new QRCode(qrcodeContainer, text);
-    }
-    generateQRCode()
+let text = "http://{{ env('APP_URL') }}:8000/sign/{{ $session->id }}"
+function generateQRCode() {
+    let qrcodeContainer = document.getElementById("qr-code");
+    qrcodeContainer.innerHTML = "";
+    new QRCode(qrcodeContainer, text);
+}
+generateQRCode()
 
-    function refreshPageData(){
-        fetch('/sessions/sign/{{$session->id}}').then(response => response.json()).then(data =>
-        {
-            console.log('data : ')
-            console.log(data)
-            const tableContainer = document.getElementById("table-container");
+function refreshPageData(){
+    fetch('/sessions/sign/{{$session->id}}').then(response => response.json()).then(data => {
+        const tableContainer = document.getElementById("table-container");
 
-
-
-        let tableHTML = `
-        <table border="1">
+        let tableHTML = '
+            <table border="1">
             <tr>
-                <th>Nom</th>
-                <th>Statut de présence</th>
+            <th>Nom</th>
+            <th>Statut de présence</th>
             </tr>
-            `;
+            ';
 
-            for(i=0; i<data.length; i++)
-            {
-                tableHTML+= `<tr>
+        for(i=0; i<data.length; i++) {
+            tableHTML+= `<tr>
                 <td>${data[i].name}</td>
                 <td>${data[i].signed ? "✅" : "❌"}</td>
             </tr>`
-            }
-            tableHTML+= `</table>`
-            tableContainer.innerHTML = tableHTML
-        })
-    }
+        }
+        tableHTML+= `</table>`
+        tableContainer.innerHTML = tableHTML
+    })
+}
 
-
-
-        setInterval(refreshPageData, 3000)
-    </script>
-    @include('footer')
+setInterval(refreshPageData, 3000)
+</script>
+@include('footer')
 
