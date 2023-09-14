c5efe54c93a521b2046f37ab3057bb9e2ae24e18
Paul OHL <paul.lv.ohl@gmail.com>
2023-09-14 16:15:55 +0200

    Small fix for phone users

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,13 +2,13 @@
 <h1>Connexion</h1>
 <form method="POST" class="row" action="/user/login">
     @csrf
+    <label for="user">Selectionnez votre utilisateur:</label>
     <div class="input-field col s12">
-        <select name="user">
+        <select class="browser-default" id="user" name="user">
             @foreach ($users as $user)
             <option value="{{ $user->id }}">{{ $user->name }} ({{ $user->status }})</option>
             @endforeach
         </select>
-        <label>Selectionnez votre utilisateur:</label>
     </div>
     <div class="input-field col s12">
         <button class="btn waves-effect waves-light" type="submit" name="action">Se connecter</button>
