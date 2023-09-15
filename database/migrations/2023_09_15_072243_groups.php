e3f95b406be968d547d53e73663d395dce4cd109
Matthias Brumant <matthias.brumant@gmail.com>
2023-09-15 10:05:05 +0200

    feat : Creation de la table 'groups' et des foreign keys necessaires

--- database/migrations/2023_09_15_072243_groups.php
+++ database/migrations/2023_09_15_072243_groups.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groups', function(Blueprint $table){
             $table->id();
             $table->string('name');
-            $table->foreign('Usergroup')->references('id')->on('groups');
             $table->timestamps();
-        });
+        }
+        );
     }
 
     /**
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
